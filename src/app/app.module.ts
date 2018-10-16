9cf6c61441b32e5ab5a55151da0947a6e818b6df
Jeff <jeffreyfang123@gmail.com>
2018-10-17 07:25:45 +0900

    Update Cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
@@ -8,6 +9,11 @@ import { TableListComponent } from './table-list/table-list.component';
 import { CardComponent } from './card/card.component';
 import { CardListComponent } from './card-list/card-list.component';
 import { XmlDataService } from './xml-data.service';
+
+
+const appRoutes: Routes = [
+  { path: 'address/:customerID', component: CardComponent },
+];
 
 @NgModule({
   declarations: [
